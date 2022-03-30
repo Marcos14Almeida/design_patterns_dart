4dda55c17d0d369122c599ed992255b4ba3bba4d
Marcos Paulo Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2022-03-30 19:30:14 -0300

    Create singleton.dart

---
+++ singleton.dart
@@ -0,0 +1,37 @@
+//https://medium.com/flutter-community/flutter-design-patterns-1-singleton-437f04e923ce
+//https://stackoverflow.com/questions/12649573/how-do-you-build-a-singleton-in-dart
+
+void main() {
+
+var s1 = Singleton(); 
+var s2 = Singleton(); 
+s1.stateName = 'Carlos';
+print(identical(s1, s2)); // true 
+print(s1 == s2); // true
+ print(s1.stateName);
+ print(s2.stateName);
+
+}
+
+abstract class abstracao{
+  late String initialName;
+  late String stateName;
+  String get currentText => stateName;
+  
+  void reset(){
+    stateName = initialName;
+  }
+}
+
+class Singleton extends abstracao{
+  static final Singleton _singleton = Singleton._internal();
+
+  factory Singleton() {   
+    return _singleton;
+  }
+
+  Singleton._internal(){
+    initialName = 'João';
+    stateName = initialName;       
+  }
+}
