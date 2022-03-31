8312cfa333d7c408b2074a0ca6f5df8573d255f8
Marcos Paulo Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2022-03-31 20:25:22 -0300

    Create builder.dart

---
+++ builder.dart
@@ -0,0 +1,6 @@
+//https://refactoring.guru/pt-br/design-patterns/builder
+
+/////////////////////////
+// BUILDER
+/////////////////////////
+//O Builder é um padrão de projeto criacional que permite a você construir objetos complexos passo a passo.
