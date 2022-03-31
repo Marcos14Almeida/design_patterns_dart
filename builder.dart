89aca907841c2e052e52a672bfbac98999588904
Marcos Paulo Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2022-03-31 20:27:35 -0300

    Update builder.dart

--- builder.dart
+++ builder.dart
@@ -4,3 +4,7 @@
 // BUILDER
 /////////////////////////
 //O Builder é um padrão de projeto criacional que permite a você construir objetos complexos passo a passo.
+
+
+//PROBLEMA
+class house(bool hasDoors, bool hasWindows, bool hasKitchen, bool hasX, bool hasY){}
